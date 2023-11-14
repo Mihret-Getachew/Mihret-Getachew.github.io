91be90d7a4c21053091f5f9c3b36225456e50585
Mihret_GetachewE <mgetachew@miu.edu>
2023-11-14 08:47:00 -0600

    assignment for grades and methods

--- w3d1arraymethods/src/grades.ts
+++ w3d1arraymethods/src/grades.ts
@@ -1,16 +1,3 @@
-/*
-This file contains  code for a quiz object. The quiz object has a key property that contains Answer objects that
-each have a property for the correct answer and another for an identifier of the question.  The way to calculate scores 
-for this data structure is to order the answers for a given student according to the qid property and do the same 
-for the key.  Then compare the two lists of answers (parallel arrays) and add up the number of matches.  
-
-You need to implement the three methods.
-
-This is a good example of using a comparator function to sort an array of objects and working with a data structure of nesed
-arrays and objects.
-
-*/
-
 type Student = { sid: number; answers: Answer[] };
 type Answer = { qid: number; ans: string };
 type Key = Answer[];
@@ -22,7 +9,7 @@ type Quiz = {
   scoreStudent: (sid: number) => number;
   getAverage: () => number;
 };
-export const quiz = {} as Quiz;
+export const quiz: Quiz = {} as Quiz;
 quiz.students = [
   {
     sid: 10,
@@ -56,31 +43,50 @@ quiz.key = [
 ];
 
 /**
- *
  * @param {Object} ans1 is an answer object
  * @param {Object} ans2 is an answer object
  * @returns {number} difference of the identifiers
  */
-function answerComparator(ans1, ans2) {
-  //IMPLEMENT THIS
+function answerComparator(ans1: Answer, ans2: Answer): number {
+  return ans1.qid - ans2.qid;
 }
 
 /**
- *
  * @param {*} sid student id
  * @returns {number} score for student
  * find this student
  * sort the student answers
  * compare them against key and add up matches
  */
-quiz.scoreStudent = function (sid) {
-  //IMPLEMENT THIS
+quiz.scoreStudent = function (sid: number): number {
+  const student = quiz.students.find((s) => s.sid === sid);
+
+  if (!student) return 0;
+
+  const sortedStudentAnswers = student.answers.slice().sort(answerComparator);
+
+  let score = 0;
+  for (let i = 0; i < quiz.key.length; i++) {
+    if (
+      sortedStudentAnswers[i] &&
+      quiz.key[i] &&
+      sortedStudentAnswers[i].ans === quiz.key[i].ans
+    ) {
+      score++;
+    }
+  }
+
+  return score;
 };
 
 /**
  * @returns {number} average score of all students
  * go through list of students and get score of each, then the average
  */
-quiz.getAverage = function () {
-  //IMPLEMENT THIS
+quiz.getAverage = function (): number {
+  const totalScores = quiz.students.reduce(
+    (sum, student) => sum + quiz.scoreStudent(student.sid),
+    0
+  );
+  return totalScores / quiz.students.length;
 };
--- w3d1arraymethods/src/methods.ts
+++ w3d1arraymethods/src/methods.ts
@@ -3,24 +3,46 @@ Write a function filterRange(arr, a, b) that gets an array arr, looks for elemen
 The function should not modify the array. It should return the new array.
 */
 export function filterRange(arr: number[], a: number, b: number): number[] {
-  return [];
+  return arr.filter((element) => element >= a && element <= b);
 }
 
 /*
 Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
 The function should only modify the array. It should not return anything.
 */
-export function filterRangeInPlace(arr: number[], a: number, b: number): void {}
+export function filterRangeInPlace(arr: number[], a: number, b: number): void {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < a || arr[i] > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
 
 type Calculator = {
   methods: { [key: string]: (a: number, b: number) => number };
   calculate: (str: string) => number;
   addMethod: (name: string, func: (a: number, b: number) => number) => void;
 };
+
 export const calculator: Calculator = {
   methods: {
     "-": (a, b) => a - b,
     "+": (a, b) => a + b,
+  },
+  calculate: function (str: string): number {
+    const values = str.split(" ");
+    const method = values[1];
+    const a = Number(values[0]);
+    const b = Number(values[2]);
+    return this.methods[method](a, b);
+  },
+
+  addMethod: function (
+    name: string,
+    func: (a: number, b: number) => number
+  ): void {
+    this.methods[name] = func;
   },
 };
 
@@ -35,10 +57,10 @@ export type User = {
 };
 
 export function groupById(users: User[]): { [key: string]: User } {
-  // declare usersById to be an object with string keys and User values
   const usersById: { [key: string]: User } = {};
-  // FURTHER IMPLEMENTATION REQUIRED HERE
-
+  for (const user of users) {
+    usersById[user.id] = user;
+  }
   return usersById;
 }
 
@@ -54,24 +76,29 @@ type FullNameUser = {
 };
 
 export function map2fullName(users: SurnameUser[]): FullNameUser[] {
-  let result: { fullName: string; id: number }[] = [];
-  // FURTHER IMPLEMENTATION REQUIRED HERE
+  let result = users.map((user) => {
+    return { fullName: `${user.name + " " + user.surname}`, id: user.id };
+  });
 
   return result;
 }
 
 export function sortByAge(users: User[]): void {
-  // FURTHER IMPLEMENTATION REQUIRED HERE
+  users.sort((a, b) => a.age - b.age);
 }
 
 export function findOldest(users: User[]): User {
   let oldest = users[0];
-  // FURTHER IMPLEMENTATION REQUIRED HERE
-
+  for (const user of users) {
+    if (user.age > oldest.age) {
+      oldest = user;
+    }
+  }
   return oldest;
 }
 
 /* getAverageAge using reduce */
 export function getAverageAge(users: User[]): number {
-  return 0;
+  const totalAge = users.reduce((sum, user) => sum + user.age, 0);
+  return totalAge / users.length;
 }
